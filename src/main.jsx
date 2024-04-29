12d46664cdbb61c968a61a971ebddee843f014a2
alex16leiva <alex16leiva@gmail.com>
2024-04-29 11:28:22 -0600

    Added styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import './index.css'
+import './style.css'
 import { TemplateApp } from './TemplateApp'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
